de9a326997b247699499783d009c76a5979e0937
masahiro96848 <feetwcpic9436@gmail.com>
2024-01-20 19:54:47 +0900

    新規登録でButtonを押した時にAPIのロジックを追加

--- src/app/(auth)/api/user/route.ts
+++ src/app/(auth)/api/user/route.ts
@@ -1,12 +1,22 @@
 import db from '@/lib/db'
 import { NextResponse } from 'next/server'
 import { hash } from 'bcrypt'
+import * as z from 'zod'
+
+const userSchema = z.object({
+  username: z.string().min(1, '名前は必須です。').max(100),
+  email: z.string().min(1, 'メールアドレスは必須です。').email('Invalid email'),
+  password: z
+    .string()
+    .min(1, 'パスワードは必須です。')
+    .min(8, 'パスワードは8文字以上で入力してください。'),
+})
 
 export async function POST(req: Request) {
   try {
     const body = await req.json()
 
-    const { email, username, password } = body
+    const { email, username, password } = userSchema.parse(body)
 
     // emailが既に存在しているかのチェック
     const existingUserByEmail = await db.user.findUnique({
